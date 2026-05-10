1629e6c39619eaa4d1f4a081d57f06e3cb90f68b
+ <+>
2026-05-10 15:59:05 -0400

    complete using array reduce

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce (reducer)
+
+console.log (`${totalBatteries}`)
+
+function reducer(a, b){
+    return a + b
+}
+
+/// youtube tutorials have been very helpful for these methods. 
